a4a1f1faee27017a71f1c4428c1d98454d535006
projectramo <projectramo@gmail.com>
2015-07-30 23:43:45 -0400

    fixed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 
+
 $('form').submit(function(event){
 	event.preventDefault();
     var $delbutton = $('<li><button class="delbutton">x</button><br></li>');
-    var $listitem = $('<div></div>')
-    var $donebutton = $('<li><button class="donebutton">' + $('#itemname').val() + '</button></li>');
+    var $listitem = $('<div class="wholeitem"></div>')
+    var $donebutton = $('<li><button class="classdonebutton">' + $('#itemname').val() + '</button></li>');
     var $itemname = $('<li>'+ $('#itemname').val() + '</li>');
     var $nameday = $('<li>yourname</li><li>datetoday</li>');
     var $undobutton = $('<li><button class="undobutton">^</button><br></li>');
@@ -14,26 +15,19 @@ $('form').submit(function(event){
     $listitem.append($delbutton);
 	$('#tbdlist').append($listitem);
 	$('input').val('');
-    $donebutton.click(function() {
-        $doneitem.append($itemname);
-        $doneitem.append($nameday);
-        $doneitem.append($undobutton);
-        $("#donelist").append($doneitem);
-        $listitem.remove();
+    $('#tbdlist').on('click', '.classdonebutton', function(e) {
+        var $localitem = $(e.target).closest('.wholeitem');
+        $("#donelist").append($localitem);
+        debugger;
+        //$localitem.remove();
     });
     $delbutton.click(function() {
         $listitem.remove();
     });
-    $undobutton.click(function(){
-    //var $delbutton = $('<li><button class="delbutton">x</button><br></li>');
-    //var $donebutton = $('<li><button class="donebutton">' + $('#itemname').val() + '</button></li>');
-    var $nameday = $('<li>yourname</li><li>datetoday</li>');
-        $listitem = $('<div></div>')
-        $listitem.append($donebutton);
-        $listitem.append($nameday);
-        $listitem.append($delbutton);
-        $('#tbdlist').append($listitem); 
-        $doneitem.remove();       
+    $('#donelist').on('click', '.classdonebutton', function(e){
+        var $localdoneitem = $(e.target).closest('.wholeitem');
+        $('#tbdlist').append($localdoneitem); 
+        //$localdoneitem.remove();       
     });
 
 });
